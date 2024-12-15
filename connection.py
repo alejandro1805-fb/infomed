1658a0696e8f4a9fc990b9c6766462b262c471d2
Manuel Sanchez <mg.sanchezb1@uniandes.edu.co>
2024-12-15 18:53:28 -0500

    ssl

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_connection():
     conn = mysql.connector.connect(
         host='ManuelGuillermoSanchez.mysql.pythonanywhere-services.com',
-        user='ManuelGuillermoSanchez',
+        user='ManuelGuillermoS',
         password='Python2024*.',
         database='ManuelGuillermoS$default',
 
