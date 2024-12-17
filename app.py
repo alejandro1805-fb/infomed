4b0191d961432a8f99c860a19acd683b7f1fa0cd
Manuel Sanchez <mg.sanchezb1@uniandes.edu.co>
2024-12-16 19:06:46 -0500

    fix get

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def add_patient():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5001)
